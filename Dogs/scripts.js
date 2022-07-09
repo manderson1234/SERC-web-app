2ba3af506cf90893c60314bf653dd024a530c058
Mark Anderson <m.anderson.uk@icloud.com>
2022-07-09 13:47:05 +0100

    finish

--- Dogs/scripts.js
+++ Dogs/scripts.js
@@ -10,31 +10,31 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true);
+request.open('GET', 'https://dog.ceo/api/breed/akita/images', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
 
   if (request.status >= 200 && request.status < 400) {
+    var images = data.message;
+
     images.forEach(image => {
         
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = "dog"
 
       const img = document.createElement('img')
-      img.src = movie.image
+      img.src = image
 
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      
 
       container.appendChild(card);
       card.appendChild(h1);
-      card.appendChild(p);
+      
       card.appendChild(img);
     });
   } else {
